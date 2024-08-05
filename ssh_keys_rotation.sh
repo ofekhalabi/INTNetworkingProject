010652ea2afa9ecd875fd64d258fed031c56739c
ofekh <halabi1205@gmail.com>
2024-08-05 21:50:17 +0300

    key_rotation

--- ssh_keys_rotation.sh
+++ ssh_keys_rotation.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
+
+
+# Paths
 PRIVATE_IP=$1
+KEY_DIR="/home/ubuntu"
+OLD_KEY="${KEY_DIR}/old_key"
+NEW_KEY="${KEY_DIR}/key_change"
+NEW_KEY_PUB="${NEW_KEY}.pub"
+
 if [[ $# -eq 1 ]]; then
-	if [[ -f /home/ubuntu/key_change ]]; then
+	if [[ -f "${KEY_DIR}/key_change" ]]; then
 		cd /home/ubuntu/
-		rm key_change
-		rm key_change.pub
-		#GENERATE SSH_KEY ON THE PUBLIC INSTANCE
-		ssh-keygen -t rsa -b 4096 -f /home/ubuntu/key_change -P "" -q
-	else
-		#GENERATE SSH_KEY ON THE PUBLIC INSTANCE
-		ssh-keygen -t rsa -b 4096 -f /home/ubuntu/key_change -P "" -q
+		if [[ -f "${OLD_KEY}" ]]; then
+			rm -f old_key
+		fi
+		mv "${KEY_DIR}/key_change" "${OLD_KEY}" #CHANGE THE NAME TO THE OLD KEY
+		rm  -f "${KEY_DIR}/key_change.pub"
 	fi
 
+	#GENERATE SSH_KEY ON THE PUBLIC INSTANCE
+	ssh-keygen -t rsa -b 4096 -f /home/ubuntu/key_change -P "" -q
+
 	#FORWARD PUBLIC KEY TO PRIVATE INSTANCE
-	scp -i /home/ubuntu/private_key_git ubuntu@localhost:/home/ubuntu/key_change.pub ubuntu@"${PRIVATE_IP}":/home/ubuntu/
+	scp -i  /home/ubuntu/old_key /home/ubuntu/key_change.pub ubuntu@"${PRIVATE_IP=}":/home/ubuntu
 
 	#APPEND THE NEW KEYS TO authorized_keys FILE
-	ssh -i /home/ubuntu/private_key_git ubuntu@"${PRIVATE_IP}" "cat key_change.pub >> .ssh/authorized_keys"
+	ssh -i /home/ubuntu/old_key ubuntu@"${PRIVATE_IP}" "cat key_change.pub >> .ssh/authorized_keys"
 
 	#CHCEK IF CAN I CONNECT TO THE MESHIN WITH THE NEW KEY
-	if [[ $(ssh -i /home/ubuntu/key_change ubuntu@"${PRIVATE_IP}" "cat key_change.pub > .ssh/authorized_keys") ]]; then
-		echo "Now you can connect to the private machine only with this key"
+	ssh -i /home/ubuntu/key_change ubuntu@"${PRIVATE_IP}" "cat key_change.pub > .ssh/authorized_keys"
+	if [[ $? -eq 0 ]]; then
+		echo "Now you can connect to the private machine only with this key: ${NEW_KEY}"
+		rm old_key
 	else
 		echo "The key you created is not good"
 	fi
@@ -28,4 +39,3 @@ else
 	echo "Please insert the private ip of your instance"
 	exit 1
 fi
-
